df40af0d24a94418ff03cac259b90a040ad8ce93
yhippa <richard.yhip@gmail.com>
2017-03-28 12:36:15 -0400

    initial commit

---
+++ src/main/java/io/github/yhippa/App.java
@@ -0,0 +1,13 @@
+package io.github.yhippa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
